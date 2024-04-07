e9bad2108f56a7ad3dcf6e2b2863a104e8eef3fe
Ryota Kimura <rkimura@fun.bio.keio.ac.jp>
2024-04-07 22:19:37 +0900

    can print w/ tview w/o color tempolary

--- catmeme_dev/go.mod
+++ catmeme_dev/go.mod
@@ -5,20 +5,24 @@ go 1.22.1
 require (
 	github.com/TheZoraiz/ascii-image-converter v1.13.1
 	github.com/gdamore/tcell v1.4.0
+	github.com/rivo/tview v0.0.0-20240406141410-79d4cc321256
 )
 
 require (
 	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.1 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/gookit/color v1.4.2 // indirect
-	github.com/lucasb-eyer/go-colorful v1.0.3 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/makeworld-the-better-one/dither/v2 v2.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.7 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/nathan-fiscaletti/consolesize-go v0.0.0-20210105204122-a87d9f614b9d // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
-	golang.org/x/sys v0.0.0-20210601080250-7ecdf8ef093b // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- catmeme_dev/printScr/printScr.go
+++ catmeme_dev/printScr/printScr.go
@@ -2,63 +2,56 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/signal"
 	"path/filepath"
 	"syscall"
 	"time"
+	"unsafe"
 
 	"github.com/TheZoraiz/ascii-image-converter/aic_package"
-	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
 )
 
+// sigintを無効にする
+// 色付き出力
+
 func main() {
-	// 初期化
-	s, err := tcell.NewScreen()
-	if err != nil {
-		log.Fatalf("%+v", err)
-	}
-	if err := s.Init(); err != nil {
-		log.Fatalf("%+v", err)
-	}
-	defer s.Fini()
-	w, h := s.Size()
-
 	sigint := make(chan os.Signal, 1)
-	signal.Notify(sigint, syscall.SIGINT)
+	signal.Notify(sigint, syscall.SIGTERM)
 
-	// 毎秒20ファイルずつ5秒間出力する
+	// get size of screen
+	w, h, err := getTerminalSize()
+	if err != nil {
+		panic(err)
+	}
+
+	// setup tview
+	app := tview.NewApplication()
+	screen := tview.NewTextView().SetScrollable(false).SetTextAlign(tview.AlignCenter)
+	screen.SetChangedFunc(func() {
+		app.Draw()
+	})
+
 	fps := 20
 	delay := time.Duration(float64(time.Second) / float64(fps))
 	seconds := 2
 	frameDir := "frames/cat_meme"
-	for i := 0; i < fps*seconds; i++ {
-		inputFilePath := filepath.Join(frameDir, fmt.Sprintf("cat_meme_%04d.jpg", i+1))
-		if asciiArt, err := processImage(inputFilePath, w-1, h-1); err != nil {
-			fmt.Printf("Error processing image %s: %v\n", inputFilePath, err)
-		} else {
-			s.Clear()
-			drawText(s, 0, 0, w, h, tcell.StyleDefault, asciiArt)
-			s.Show()
+	go func() {
+		for i := 0; i < fps*seconds; i++ {
+			inputFilePath := filepath.Join(frameDir, fmt.Sprintf("cat_meme_%04d.jpg", i+1))
+			if asciiArt, err := processImage(inputFilePath, w-1, h-1); err != nil {
+				fmt.Printf("Error processing image %s: %v\n", inputFilePath, err)
+			} else {
+				screen.SetText(asciiArt)
+			}
 			time.Sleep(delay)
 		}
-	}
-}
+		app.Stop()
+	}()
 
-func drawText(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, text string) {
-	row := y1
-	col := x1
-	for _, r := range []rune(text) {
-		s.SetContent(col, row, r, nil, style)
-		col++
-		if col >= x2 {
-			row++
-			col = x1
-		}
-		if row > y2 {
-			break
-		}
+	if err := app.SetRoot(screen, true).Run(); err != nil {
+		panic(err)
 	}
 }
 
@@ -68,10 +61,26 @@ func processImage(inputPath string, w, h int) (string, error) {
 	flags.Dimensions = []int{w, h}
 	// flags.Colored = true
 	// flags.SaveTxtPath = "."
-	// flags.CustomMap = " .-=+#@"
 	// flags.SaveBackgroundColor = [4]int{}
 	// flags.SaveGifPath = "ascii_arts"
 	asciiArt, err := aic_package.Convert(inputPath, flags)
 
 	return asciiArt, err
+}
+
+func getTerminalSize() (width, height int, err error) {
+	var dimensions [4]uint16
+	_, _, errno := syscall.Syscall(
+		syscall.SYS_IOCTL,
+		os.Stdout.Fd(),
+		syscall.TIOCGWINSZ,
+		uintptr(unsafe.Pointer(&dimensions)),
+	)
+	if errno != 0 {
+		err = errno
+		return
+	}
+	width = int(dimensions[1])
+	height = int(dimensions[0])
+	return
 }
